3cb3149b12f66dcb16fb5b1e218aba707b5fdafe
Keshapeddi <pramod-kumar.keshapeddi@capgemini.com>
2018-02-13 12:57:23 +0530

    added firstgradleproject to github

---
+++ FirstGradleProject/src/main/java/com/capgemini/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.capgemini;
+
+public class HelloWorld
+{
+  public static void main(String args[])
+  
+	{
+
+     		System.out.println("hello world .. my first gradle program !!!!!");
+	}
+}
